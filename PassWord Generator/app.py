7e2c4bc4223ff1dcc63b48c7fc3562aefdb5108f
joyatee-datta-2020 <joyetaratri123@gmail.com>
2021-10-07 01:19:36 +0600

    add qrcode to password generator

--- PassWord Generator/app.py
+++ PassWord Generator/app.py
@@ -1,7 +1,11 @@
 import random
+import qrcode
 
 inp = int(input("Enter the length of password : "))
 string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 gen_pass =  "".join(random.sample(string, inp))
 
 print("Random generated password : ", gen_pass)
+
+image = qrcode.make(gen_pass)
+image.save("PASSWORD.jpg")
